b4461388aa9c82bd0994dcd9174dcf9190574c28
Bartłomiej Kulesa <bartekbpk@gmail.com>
2017-06-28 20:05:08 +0200

    Add class GamePlayScreen extends AbstractScreen

---
+++ core/src/com/gmail/bartekbpk/com/gmail/bartekbpk/screens/GamePlayScreen.java
@@ -0,0 +1,14 @@
+package com.gmail.bartekbpk.com.gmail.bartekbpk.screens;
+
+import com.gmail.bartekbpk.TutorialClickerGame;
+
+public class GamePlayScreen extends AbstractScreen {
+    public GamePlayScreen(TutorialClickerGame game) {
+        super(game);
+        initial();
+    }
+
+    private void initial() {
+
+    }
+}
