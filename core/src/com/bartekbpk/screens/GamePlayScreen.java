54293f89d130f2b41c66ea82b17df7d56a873650
Bartłomiej Kulesa <bartekbpk@gmail.com>
2017-06-29 20:12:53 +0200

    Dodany button do GamePlayScreen

--- core/src/com/bartekbpk/screens/GamePlayScreen.java
+++ core/src/com/bartekbpk/screens/GamePlayScreen.java
@@ -1,11 +1,15 @@
 package com.bartekbpk.screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.bartekbpk.game.TutorialClickerGame;
 import com.bartekbpk.entities.Player;
 
 public class GamePlayScreen extends com.bartekbpk.screens.AbstractScreen {
 
     private Player player;
+    private Button playerButton;
 
     public GamePlayScreen(TutorialClickerGame game) {
         super(game);
@@ -14,6 +18,27 @@ public class GamePlayScreen extends com.bartekbpk.screens.AbstractScreen {
     @Override
     protected void initial() {
         initPlayer();
+        initPlauerButton();
+    }
+
+    private void initPlauerButton() {
+        playerButton = new Button(new Button.ButtonStyle());
+        playerButton.setWidth(460);
+        playerButton.setHeight(360);
+        playerButton.setX(10);
+        playerButton.setY(170);
+        playerButton.setDebug(true); // Jeżeli button jest niewidoczny to pojawia się ramka.
+
+        stage.addActor(playerButton);
+
+        playerButton.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+
+                System.out.println("click");
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
     }
 
     private void initPlayer() {
