5d7443e7f3f64db070e0e5bf66967fc81dc7d3b4
Bartłomiej Kulesa <bartekbpk@gmail.com>
2017-07-02 22:41:07 +0200

    End tutorial 4.

--- core/src/com/bartekbpk/entities/Player.java
+++ core/src/com/bartekbpk/entities/Player.java
@@ -4,6 +4,7 @@ package com.bartekbpk.entities;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
@@ -26,6 +27,11 @@ public class Player extends Image {
 
     public void reactOnClick() {
         int moveAmount = MathUtils.random(-130, 130);
-        this.addAction(Actions.moveBy(moveAmount, 10, 3, Interpolation.circle));
+        Action moveAction = Actions.sequence(
+                Actions.moveBy(moveAmount, 10, 3, Interpolation.circle),
+                Actions.moveBy(-moveAmount, -10, 3, Interpolation.exp10In)
+        );
+
+        this.addAction(moveAction);
     }
 }
