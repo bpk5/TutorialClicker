68a97429fd200b8d301d9d7f708848a8231040d2
Bartłomiej Kulesa <bartekbpk@gmail.com>
2017-07-03 20:13:44 +0200

    Reset player score. Tutorial 6 - end.

--- core/src/com/bartekbpk/game/TutorialClickerGame.java
+++ core/src/com/bartekbpk/game/TutorialClickerGame.java
@@ -1,11 +1,16 @@
 package com.bartekbpk.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.bartekbpk.screens.SplachScreen;
 
 
-// TODO: 29.06.17 tutorial 4. 15 minut
+// TODO: 29.06.17 tutorial 6
 public class TutorialClickerGame extends Game {
+
+    public final static String GAME_PREFS = "com.bartekbpk.game.prefs";
+    public final static String GAME_SCORE = "com.bartekbpk.game.prefs.score";
 
     public final static String GAME_NAME = "Tutorial Clicker";
 
@@ -14,16 +19,39 @@ public class TutorialClickerGame extends Game {
 
     private  boolean paused; // Flag: true - game stopped, false - game play
 
+    private Preferences prefs;
+
     private int point;
 	
 	@Override
 	public void create () {
+        initial();
         // To splachScrean we pass this object.
         this.setScreen(new SplachScreen(this));
 	}
 
-	public void addPoint() {
+    private void initial() {
+        prefs = Gdx.app.getPreferences(GAME_PREFS);
+        loadScore();
+    }
+
+    private void loadScore() {
+        point = prefs.getInteger(GAME_SCORE);
+    }
+
+    public void addPoint() {
         point++;
+        updateSavedScoreInPrefs();
+    }
+
+    public void resetGameScore() {
+        point = 0;
+        updateSavedScoreInPrefs();
+    }
+
+    private void updateSavedScoreInPrefs() {
+        prefs.putInteger(GAME_SCORE, point);
+        prefs.flush();
     }
 
     /**
--- core/src/com/bartekbpk/screens/GamePlayScreen.java
+++ core/src/com/bartekbpk/screens/GamePlayScreen.java
@@ -12,6 +12,7 @@ public class GamePlayScreen extends AbstractScreen {
 
     private Player player;
     private Button playerButton;
+    private Button resetScoreButton;
     private Label scoreLabel;
 
     public GamePlayScreen(TutorialClickerGame game) {
@@ -22,7 +23,27 @@ public class GamePlayScreen extends AbstractScreen {
     protected void initial() {
         initPlayer();
         initPlauerButton();
+        initResetScoreButton();
         initScoreLabel();
+    }
+
+    private void initResetScoreButton() {
+        resetScoreButton = new Button(new Button.ButtonStyle());
+        resetScoreButton.setWidth(100);
+        resetScoreButton.setHeight(100);
+        resetScoreButton.setX(330);
+        resetScoreButton.setY(560);
+        resetScoreButton.setDebug(true);
+
+        stage.addActor(resetScoreButton);
+
+        resetScoreButton.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.resetGameScore();
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
     }
 
     private void initScoreLabel() {
